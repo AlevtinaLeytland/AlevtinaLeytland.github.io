5528687e73408f9fb14a862303da0dd5a8dff007
AlevtinaLeytland <clavusclavo321@gmail.com>
2023-05-23 13:44:51 +0200

    changing API init

--- api/api.js
+++ api/api.js
@@ -1,4 +1,4 @@
-const newsKey = "91436f93d637403d88e7a6eda1dcc7fa";
+const newsKey = "V22WNCFJ9uBUYoAogP8L112wDfKMExF8zZaAvXxPucE";
 
 const quoteUrl = "https://api.quotable.io";
 
@@ -6,18 +6,25 @@ export async function getRandomQuote() {
   const data = await fetch(`${quoteUrl}/random`);
   return await data.json();
 }
-const baseUrlNews = `https://newsapi.org/v2/`;
+const baseUrlNews = `https://api.newscatcherapi.com/v2/`;
 
-export async function getNewsBySelect(country, category) {
+export async function getNewsBySelect(keywords = "Tesla", country, category) {
   const data = await fetch(
-    `${baseUrlNews}top-headlines?country=${country}&category=${category}&apiKey=${newsKey}`,
+    `${baseUrlNews}search?q=${keywords}&countries=${country}&topic=${category}`,
+    {
+      headers: {
+        "x-api-key": newsKey,
+      },
+    },
   );
   return await data.json();
 }
 
 export async function getNewsBySearch(text) {
-  const data = await fetch(
-    `${baseUrlNews}everything?q=${text}&apiKey=${newsKey}`,
-  );
+  const data = await fetch(`${baseUrlNews}search?q=${text}`, {
+    headers: {
+      "x-api-key": newsKey,
+    },
+  });
   return await data.json();
 }
